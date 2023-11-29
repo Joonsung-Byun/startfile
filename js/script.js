7d26a5c21be272b67531d10e5a293feb318d252f
Joonsung-Byun <122327073+Joonsung-Byun@users.noreply.github.com>
2023-11-28 21:41:26 -0700

    asdasd

--- js/script.js
+++ js/script.js
@@ -1,19 +1,12 @@
+
+
 const ph = document.querySelector('#primaryHeader')
 
-const btn = document.querySelector('button')
+const btn = document.querySelector('#btn')
 
 const nav = document.querySelector('#primaryNav')
-console.log(nav)
 
-let isOpen = false; //오픈인가요? false입니다
 
-btn.addEventListener('click', () => {
-    if (isOpen) {
-        nav.style.transition = 'transform 0.3s ease';
-        nav.style.transform = `translateX(0%)`;
-    } else {
-        nav.style.transform = ''; // 이 부분은 transform 스타일을 초기화하여 이전 상태로 돌아갑니다.
-    }
-    isOpen = !isOpen;
-});
-
+btn.addEventListener('click', ()=>{
+    nav.classList.toggle('open')
+})
